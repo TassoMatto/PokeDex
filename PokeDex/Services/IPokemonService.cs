bd278102ea9ec0a807303c05a78d7d64174ed3dd
Simone Tassotti <>
2025-05-08 11:12:48 +0200

    Remove old Service

---
+++ PokeDex/Services/IPokemonService.cs
@@ -0,0 +1,11 @@
+using PokeDex.Models;
+
+namespace PokeDex.Services
+{
+    public interface IPokemonService
+    {
+        public Task<T?> GetPokemon<T>(uint offset = 0, uint limit = 20);
+        
+        public Task<T?> GetPokemonAbility<T>(string url_ability);
+    }
+}
